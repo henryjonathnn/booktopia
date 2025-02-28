b4668afe025aa9d93d0272e3e224cf47683153f2
henryjonathnn <henryjonathanchandra12@gmail.com>
2025-02-28 21:25:51 +0700

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? 'Booktopia' }}</title>
+    <link rel="shortcut icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
     @once
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="{{ mix('resources/js/app.js') }}" defer></script>
